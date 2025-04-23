ff7b9f7a36e1fee15b17cc2aebd47c3b87d3b054
ragnarW7 <110658242+vamshixg9@users.noreply.github.com>
2025-04-23 15:48:12 +0900

    add dynamic dashboard

---
+++ app/realDawg/logs.js
@@ -0,0 +1,20 @@
+const logs = [
+    {
+        Date: '2025/04/23',
+        ClockIn: '08:46',
+        ClockOut: '17:45',
+        TotalHrs: 'Active'
+    },
+    {
+        Date: '2025/04/22',
+        ClockIn: '08:45',
+        ClockOut: '17:45',
+        TotalHrs: '07:45min'
+    },
+    {
+        Date: '2025/04/21',
+        ClockIn: '09:00',
+        ClockOut: '17:45',
+        TotalHrs: '07:45min'
+    },
+]
